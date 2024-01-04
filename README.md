1b404a7e9eee93c4c92bf853e37b3f22c221422e
Wesley Noble <wrn007@hotmail.com>
2024-01-04 04:45:23 -0500

    added comments and fully finalized code. Updated Read.me file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # weatherapp
 A web application that can show you the weather in the inputted area.
+This features a search bar to find the weather for a specific city. recently searched cities will appear in a search history that can be clicked on to automatically research that city. 
+
+It displays the current weather for that day, as well as the next 5 days. The stats displayed are the temp, wind speed and humidity, along with a small icon to show the general weather.
+
+
+![Alt text](assets/images/Website.png)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,51 @@
+// Get references to HTML elements
 let city = document.getElementById("citytext");
 let searchBtn = document.getElementById("searchbtn");
 let apiKey = "412c294f475f2927ec8a04b5412df1e6";
 let cityChosen = city.value.trim();
 
+// on button click
 function preSearch() {
     cityChosen = city.value.trim();
 
+    // Check if the city input is empty
     if (cityChosen === "") {
         window.alert('Please choose a valid city');
     } else {
+        // Trigger the weather search and update history
         searchCity(cityChosen);
         addItem(cityChosen);
     }
 }
 
+// add a city to the search history
 function addItem(cityName) {
+    // Get existing items from local storage
     var items = JSON.parse(localStorage.getItem('items')) || [];
 
+    // Add the new item to the array
     if (typeof cityName === 'string') {
         items.push(cityName);
+
+        // Save
         localStorage.setItem('items', JSON.stringify(items));
+
+        // Update the displayed list
         updateList();
     } else {
         console.error('Error :(');
     }
 }
 
+// update the displayed search history list
 function updateList() {
     var itemList = document.getElementById('history');
-    itemList.innerHTML = '';
+    itemList.innerHTML = ''; // Clear the current list
 
+    // Get the items from local storage
     var items = JSON.parse(localStorage.getItem('items')) || [];
 
+    // Populate the list with items
     items.forEach(function (item) {
         var button = document.createElement('button');
         button.textContent = item;
@@ -45,8 +59,10 @@ function updateList() {
     });
 }
 
+// Initial update of the list when the page loads
 updateList();
 
+// search for weather data of a city
 function searchCity(cityToSearch) {
     fetch('https://api.openweathermap.org/geo/1.0/direct?q=' + cityToSearch + '&limit=1&appid=' + apiKey)
         .then(function (response) {
@@ -56,6 +72,7 @@ function searchCity(cityToSearch) {
             if (data.length > 0) {
                 longitude = data[0].lon;
                 latitude = data[0].lat;
+                // Display current weather and 5-day forecast
                 displayDataCur(latitude, longitude);
                 display5day(latitude, longitude);
             } else {
@@ -64,19 +81,24 @@ function searchCity(cityToSearch) {
         });
 }
 
+// display current weather data
 function displayDataCur(latitude, longitude) {
     fetch('https://api.openweathermap.org/data/2.5/weather?units=metric&lat=' + latitude + "&lon=" + longitude + "&appid=" + apiKey)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
+            // Format the current date
             let dayjsDate = dayjs().format('MMM-D-YYYY');
+            // Get references
             let cityName = document.getElementById('cityName');
             let curDate = document.getElementById('curDate');
             let curTemp = document.getElementById('curTemp');
             let curWind = document.getElementById('curWind');
             let curHum = document.getElementById('curHum');
             let curIcon = document.getElementById('curIcon');
+
+            // Update HTML elements with received weather data
             cityName.textContent = data.name;
             curDate.textContent = dayjsDate;
             curTemp.textContent = "Temperature: " + data.main.temp + "°C";
@@ -86,30 +108,39 @@ function displayDataCur(latitude, longitude) {
         });
 }
 
+// display 5-day forecast data
 function display5day(latitude, longitude) {
     fetch('https://api.openweathermap.org/data/2.5/forecast?units=metric&lat=' + latitude + "&lon=" + longitude + "&appid=" + apiKey)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
+            // Get the list
             let daysList = data.list;
+
+            // Loop through the forecast data
             for (let i = 0; i < daysList.length; i += 8) {
                 let j = i / 8;
                 let dateOnly = daysList[i].dt_txt;
                 dateOnly = dateOnly.split(" ");
                 dateOnly = dateOnly[0];
+
+                // Get references
                 let date = document.getElementById('date' + j);
-                date.textContent = dateOnly;
                 let temperature = document.getElementById('temp' + j);
-                temperature.textContent = 'Temp: ' + daysList[i].main.temp;
                 let wind = document.getElementById('wind' + j);
-                wind.textContent = 'Wind Speed: ' + daysList[i].wind.speed;
                 let humidity = document.getElementById('hum' + j);
-                humidity.textContent = 'Humidity: ' + daysList[i].main.humidity + "%";
                 let icon = document.getElementById('icon' + j);
+
+                // Update HTML elements
+                date.textContent = dateOnly;
+                temperature.textContent = 'Temp: ' + daysList[i].main.temp;
+                wind.textContent = 'Wind Speed: ' + daysList[i].wind.speed;
+                humidity.textContent = 'Humidity: ' + daysList[i].main.humidity + "%";
                 icon.src = 'https://openweathermap.org/img/w/' + daysList[i].weather[0].icon + '.png';
             }
         });
 }
 
+// search button
 searchBtn.addEventListener("click", preSearch);
